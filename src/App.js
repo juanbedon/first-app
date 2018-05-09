312bc0d279b4ab778a0929946ae1fb0e4e3554a6
Juan <juanbedon26@hotmail.com>
2018-05-09 16:33:22 -0500

    Changestate  title with button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.changeTitle = this.changeTitle.bind(this)
+
+    this.state = {
+      title: "Hola Mundo desde Estado"
+    }
+  }
+
   render() {
     return (
-      <h1>Hola Mundo </h1>
+      <div>
+        <h1>{this.state.title}</h1>
+        <button onClick={this.changeTitle}>Cambia el título</button>
+      </div>
     );
   }
+
+  changeTitle(){
+    this.setState({
+      title: "Nuevo título"
+    });
+  }
+
 }
 
 export default App;
